a5c9fde5924183b02e2d4348c703f6ccfbecccd2
programthis <kylebachan@gmail.com>
2014-03-28 22:49:44 -0400

    Updated books Controller with index, show, create, edit, destroy methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,48 @@
 class BooksController < ApplicationController
   def index
-  	
+  	@book = Book.all
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  	@scene = Scene.new
+  end
+
+  def new
+  	@book = Book.new
   end
 
   def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  		redirect_to root_path
+  	else
+  		render :new
+  	end
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update_attributes(book_params)
+  		redirect_to book_path(params[:id])
+  	else
+  		render :edit
+  	end
   end
 
   def delete
+  	@book = Book.find(params[:id])
+  	@book.destroy
+  	redirect_to root_path
   end
+
+  private
+  def book_params
+  	params.require(:book).permit(:name, :author, :ISBN)
+  end
+
 end
