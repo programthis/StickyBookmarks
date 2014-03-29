050688079513c139fa8740206ebce6334e4d8b01
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-03-29 11:03:07 -0400

    Added user mailer for password reset, all through sorcery. Also added a users_form for our user base

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
 
 	def index
 	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def destroy
+		@user.destroy
+		redirect_to :back
+	end
+
 end
