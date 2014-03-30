b8f39f7619842b97e03e33fe5dceb41b21e0bd3c
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-03-30 11:33:25 -0400

    Changed redirect path to book show. Also added more CSS changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
     @book.ISBN = cover["items"][0]["volumeInfo"]["industryIdentifiers"][1]["identifier"]
 
   	if @book.save
-  		redirect_to root_path
+  		redirect_to book_path(@book)
   	else
   		render :new
   	end
